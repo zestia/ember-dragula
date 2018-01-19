78cf1967473e69773f2de1004a276d66e73981b8
Andrew Kirwin <andrew.kirwin@zestia.com>
2018-01-19 13:02:23 +0000

    cleanup

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function(environment) {
-  let ENV = {
+  const ENV = {
     modulePrefix: 'dummy',
     environment,
     rootURL: '/',
@@ -11,10 +11,7 @@ module.exports = function(environment) {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
       },
-      EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
-        Date: false
-      }
+      EXTEND_PROTOTYPES: false
     },
 
     APP: {
