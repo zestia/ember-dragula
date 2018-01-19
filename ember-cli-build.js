af217e7b09f9165721901219f67e28019c1abd94
Andrew Kirwin <andrew.kirwin@zestia.com>
2018-01-19 11:42:49 +0000

    run ember init

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,4 +1,3 @@
-/* eslint-env node */
 'use strict';
 
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
@@ -6,9 +5,6 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
     // Add options here
-    'ember-cli-babel': {
-      includePolyfill: true
-    }
   });
 
   /*
