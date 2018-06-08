b367fb0a0c8e82df116f91894e676446b554b441
mikerhyssm <mike.smith@zestia.com>
2018-06-08 15:31:28 +0100

    WIP attempt typescript 2

---
+++ addon/components/ember-dragula-container.ts
@@ -0,0 +1,22 @@
+import Component from "@ember/component";
+// @ts-ignore
+import layout from "@zestia/ember-dragula/templates/components/ember-dragula-container";
+
+export default class EmberDragulaContainer extends Component.extend( {
+  classNames: ["ember-dragula-container"],
+  layout,
+}) {
+  public onInsert: (container: EmberDragulaContainer) => void;
+  public onDestroy: (container: EmberDragulaContainer) => void;
+
+  public didInsertElement(this: EmberDragulaContainer) {
+    this._super(...arguments);
+    this.onInsert(this);
+  }
+
+  public willDestroyElement(this: EmberDragulaContainer) {
+    this._super(...arguments);
+    this.onDestroy(this);
+  }
+
+}
