e40b0c62ad90ddb5ddf8feea1419260117b991c9
mikerhyssm <mike.smith@zestia.com>
2018-06-08 17:28:11 +0100

    rewrite test utils in TS

---
+++ addon/utils/simulate-drag-drop.ts
@@ -0,0 +1,51 @@
+function _fireMouseEvent(type: string, elem: HTMLElement, centerX: number, centerY: number): void {
+  const evt = document.createEvent("MouseEvents");
+  evt.initMouseEvent(type, true, true, window, 1, 1, 1,
+    centerX, centerY, false, false, false, false, 0, elem);
+  elem.dispatchEvent(evt);
+}
+
+export function simulateDragAndDrop(elemDrag: HTMLElement, elemDrop: HTMLElement): void {
+  if (!elemDrag || !elemDrop) {
+    return;
+  }
+
+  const pos = elemDrag.getBoundingClientRect();
+  const centerDragX = Math.floor((pos.left + pos.right) / 2);
+  const centerDragY = Math.floor((pos.top + pos.bottom) / 2);
+
+  const dropPos = elemDrop.getBoundingClientRect();
+  const centerDropX = Math.floor((dropPos.left + dropPos.right) / 2);
+  const centerDropY = Math.floor((dropPos.top + dropPos.bottom) / 2);
+
+  _fireMouseEvent("mousedown", elemDrag, centerDragX, centerDragY);
+  _fireMouseEvent("mousemove", elemDrag, centerDragX + 1, centerDragY + 1 );
+  _fireMouseEvent("mousemove", elemDrag, centerDropX, centerDropY);
+  _fireMouseEvent("mouseup", elemDrag, centerDropX, centerDropY);
+}
+
+export function simulateDrag(elemDrag: HTMLElement): void {
+  if (!elemDrag) {
+    return;
+  }
+
+  const pos = elemDrag.getBoundingClientRect();
+  const centerDragX = Math.floor((pos.left + pos.right) / 2);
+  const centerDragY = Math.floor((pos.top + pos.bottom) / 2);
+
+  _fireMouseEvent("mousedown", elemDrag, centerDragX, centerDragY);
+  _fireMouseEvent("mousemove", elemDrag, centerDragX + 1, centerDragY + 1 );
+}
+
+export function simulateDrop(elemDrag: HTMLElement, elemDrop: HTMLElement): void {
+  if (!elemDrag || !elemDrop) {
+    return;
+  }
+
+  const pos = elemDrop.getBoundingClientRect();
+  const centerDropX = Math.floor((pos.left + pos.right) / 2);
+  const centerDropY = Math.floor((pos.top + pos.bottom) / 2);
+
+  _fireMouseEvent("mousemove", elemDrag, centerDropX, centerDropY);
+  _fireMouseEvent("mouseup", elemDrag, centerDropX, centerDropY);
+}
