c81a98efac2b7dba1e20a891070f4c3d5a5df864
mikerhyssm <mike.smith@zestia.com>
2018-06-18 16:22:38 +0100

    rewrite test dummy app in TS

---
+++ tests/dummy/app/components/test-options.ts
@@ -0,0 +1,30 @@
+/* tslint:disable no-console */
+import Component from "@ember/component";
+
+export default class TestOptions extends Component {
+
+    public list = [
+      { name: "Item 1" },
+      { name: "Item 2" },
+      { name: "Item 3" },
+    ];
+    public listTwo = [
+      { name: "Item 4" },
+      { name: "Item 5" },
+      { name: "Item 6" },
+    ];
+
+    public actions = {
+      dropped() {
+        console.log("Item Dropped");
+      },
+
+      dragged() {
+        console.log("Item Dragged");
+      },
+    };
+
+    public moves() {
+      return false;
+    }
+}
