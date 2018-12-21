f545be1e20537f9d5bff58883b09870c9165ff4e
Andrew Kirwin <andrew.kirwin@zestia.com>
2018-12-21 13:14:20 +0000

    fix tests in older versions of ember

--- tests/acceptance/ember-dragula-test.js
+++ tests/acceptance/ember-dragula-test.js
@@ -10,29 +10,29 @@ module('Acceptance | ember-dragula', function(hooks) {
     await visit('/');
 
     assert.equal(currentURL(), '/');
-    assert.equal(findAll('#list-1 .item')[0].textContent, 'Item 1');
-    assert.equal(findAll('#list-1 .item')[1].textContent, 'Item 2');
-    assert.equal(findAll('#list-1 .item')[2].textContent, 'Item 3');
-    assert.equal(findAll('#list-2 .item')[0].textContent, 'Item 4');
-    assert.equal(findAll('#list-2 .item')[1].textContent, 'Item 5');
-    assert.equal(findAll('#list-2 .item')[2].textContent, 'Item 6');
+    assert.equal(findAll('.list-1 .item')[0].textContent, 'Item 1');
+    assert.equal(findAll('.list-1 .item')[1].textContent, 'Item 2');
+    assert.equal(findAll('.list-1 .item')[2].textContent, 'Item 3');
+    assert.equal(findAll('.list-2 .item')[0].textContent, 'Item 4');
+    assert.equal(findAll('.list-2 .item')[1].textContent, 'Item 5');
+    assert.equal(findAll('.list-2 .item')[2].textContent, 'Item 6');
   });
 
   test('dragging objects', async function(assert) {
     await visit('/');
 
     assert.equal(currentURL(), '/');
-    const elemDrag = findAll('#list-1 .item')[0];
+    const elemDrag = findAll('.list-1 .item')[0];
 
-    const elemDrop = find('#list-2');
+    const elemDrop = find('.list-2');
 
-    assert.equal(findAll('#list-1 .item').length, 3);
-    assert.equal(findAll('#list-2 .item').length, 3);
+    assert.equal(findAll('.list-1 .item').length, 3);
+    assert.equal(findAll('.list-2 .item').length, 3);
 
     simulateDragAndDrop(elemDrag, elemDrop);
 
-    assert.equal(findAll('#list-1 .item').length, 2);
-    assert.equal(findAll('#list-2 .item').length, 4);
+    assert.equal(findAll('.list-1 .item').length, 2);
+    assert.equal(findAll('.list-2 .item').length, 4);
   });
 
   test('accepts dragula options', async function(assert) {
@@ -42,28 +42,28 @@ module('Acceptance | ember-dragula', function(hooks) {
 
     // Test passing copy option
 
-    const elemDrag = findAll('#list-copy-1 .item')[0];
-    const elemDrop = find('#list-copy-2');
+    const elemDrag = findAll('.list-copy-1 .item')[0];
+    const elemDrop = find('.list-copy-2');
 
-    assert.equal(findAll('#list-copy-1 .item').length, 3);
-    assert.equal(findAll('#list-copy-2 .item').length, 3);
+    assert.equal(findAll('.list-copy-1 .item').length, 3);
+    assert.equal(findAll('.list-copy-2 .item').length, 3);
 
     simulateDragAndDrop(elemDrag, elemDrop);
 
-    assert.equal(findAll('#list-copy-1 .item').length, 3);
-    assert.equal(findAll('#list-copy-2 .item').length, 4);
+    assert.equal(findAll('.list-copy-1 .item').length, 3);
+    assert.equal(findAll('.list-copy-2 .item').length, 4);
 
     // Test passing move function as option
 
-    const unDraggableElem = findAll('#list-moves-1 .item')[0];
-    const unDraggableElemDrop = find('#list-moves-2');
+    const unDraggableElem = findAll('.list-moves-1 .item')[0];
+    const unDraggableElemDrop = find('.list-moves-2');
 
-    assert.equal(findAll('#list-moves-1 .item').length, 3);
-    assert.equal(findAll('#list-moves-2 .item').length, 3);
+    assert.equal(findAll('.list-moves-1 .item').length, 3);
+    assert.equal(findAll('.list-moves-2 .item').length, 3);
 
     simulateDragAndDrop(unDraggableElem, unDraggableElemDrop);
 
-    assert.equal(findAll('#list-moves-1 .item').length, 3);
-    assert.equal(findAll('#list-moves-2 .item').length, 3);
+    assert.equal(findAll('.list-moves-1 .item').length, 3);
+    assert.equal(findAll('.list-moves-2 .item').length, 3);
   });
 });
