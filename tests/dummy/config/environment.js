f89b355a1b6469ca01ab7607bc37b73803893ee6
Andrew Kirwin <andrew.kirwin@zestia.com>
2019-05-20 16:56:29 +0100

    correct path

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -42,7 +42,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.locationType = 'hash';
-    ENV.rootURL = '/ember-select-box/';
+    ENV.rootURL = '/ember-dragula/';
   }
 
   return ENV;
