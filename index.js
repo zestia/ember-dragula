0fea5a5b9a5b4233f3b5718b4543918b9ad1c971
Andrew Kirwin <andrew.kirwin@zestia.com>
2019-06-04 11:40:33 +0100

    upgrade deps and fix misconfigured ember-auto-import

--- index.js
+++ index.js
@@ -3,7 +3,16 @@
 module.exports = {
   name: '@zestia/ember-dragula',
 
+  options: {
+    autoImport: {
+      alias: {
+        dragula: 'dragula/dist/dragula.js'
+      }
+    }
+  },
+
   included(app) {
-    app.import('node_modules/dragula/dist/dragula.min.css');
+    this._super.included.apply(this, arguments);
+    app.import('node_modules/dragula/dist/dragula.css');
   }
 };
